583d90395ae69716dfac2f2cdbb34a950914639a
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-04 23:53:47 -0300

    condicoes1

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+
+var vel = 12
+console.log(`A velocidade é ${vel}Km/h`)
+if (vel > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade máxima. Você será multado`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
